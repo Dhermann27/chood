b97fc85528745113a72f7260385e2517c0d237b1
Dan Hermann <dhermann27@gmail.com>
2025-01-06 11:50:51 -0600

    Update note for Rock

--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -6,11 +6,11 @@ sudo apt update && sudo apt install cec-utils && sudo apt install watchdog && su
 crontab
 5X 5 * * * sudo reboot
 
-echo -e "CONF_SWAPFILE=/home/chood/swapfile\nCONF_SWAPSIZE=1024" | sudo tee -a /etc/dphys-swapfile > /dev/null && sudo systemctl restart dphys-swapfile
+echo -e "CONF_SWAPFILE=/home/rock/swapfile\nCONF_SWAPSIZE=1024" | sudo tee -a /etc/dphys-swapfile > /dev/null && sudo systemctl restart dphys-swapfile
 
-echo -e "[Unit]\nDescription=Relaunch Browser\nAfter=multi-user.target\nWants=graphical.target\n\n[Service]\nEnvironment=\"XAUTHORITY=/home/chood/.Xauthority\"\nEnvironment=\"DISPLAY=:0\"\nExecStartPre=/bin/sleep 15\nExecStart=/bin/bash -c \"midori -e Fullscreen --display=:0 https://www.cbwcw.co/$(hostname)\"\nExecStartPost=/bin/bash -c 'sleep 15 && xdotool search --sync --onlyvisible --class midori windowactivate key Tab'\nUser=rock\nGroup=rock\nRestart=always\nRestartSec=5\nRuntimeMaxSec=3595\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/relaunch_browser.service > /dev/null
+echo -e "[Unit]\nDescription=Relaunch Browser\nAfter=multi-user.target\nWants=graphical.target\n\n[Service]\nEnvironment=\"XAUTHORITY=/home/rock/.Xauthority\"\nEnvironment=\"DISPLAY=:0\"\nExecStartPre=/bin/sleep 15\nExecStart=/bin/bash -c \"midori -e Fullscreen --display=:0 https://www.cbwcw.co/$(hostname)\"\nExecStartPost=/bin/bash -c 'sleep 15 && xdotool search --sync --onlyvisible --class midori windowactivate key Tab'\nUser=rock\nGroup=rock\nRestart=always\nRestartSec=5\nRuntimeMaxSec=3595\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/relaunch_browser.service > /dev/null
 
-sudo systemctl daemon-reload && sudo systemctl enable relaunch_browser.service && touch /home/chood/.Xauthority && sudo systemctl start relaunch_browser.service
+sudo systemctl daemon-reload && sudo systemctl enable relaunch_browser.service && touch /home/rock/.Xauthority && sudo systemctl start relaunch_browser.service
 
 sudo reboot
 </pre>
