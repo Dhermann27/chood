1473ae78af7a76f45f12d0c24135f8c2592a4ed1
Dan Hermann <dhermann27@gmail.com>
2025-05-10 08:38:44 -0500

    New branding and Weird Booking

--- app/Jobs/GoFetchBookingJob.php
+++ app/Jobs/GoFetchBookingJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Dog;
 use App\Models\DogService;
 use App\Models\Service;
 use App\Services\FetchDataService;
@@ -11,6 +12,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class GoFetchBookingJob implements ShouldQueue
 {
@@ -45,18 +47,23 @@ class GoFetchBookingJob implements ShouldQueue
         $response = $fetchDataService->fetchData(config('services.dd.uris.booking') . $this->bookingId, $payload)
             ->getData(true);
         if (isset($response['data']) && is_array($response['data']) && count($response['data']) > 0) {
-            $specialServiceMap  = Service::whereIn('category', config('services.dd.special_service_cats'))
+            $existingPetIds = Dog::pluck('pet_id')->flip(); // fast lookup
+            $specialServiceMap = Service::whereIn('category', config('services.dd.special_service_cats'))
                 ->pluck('id', 'dd_id');
             $dogs = collect($response['data'])->groupBy('contactId');
             foreach ($dogs as $petId => $services) {
-                foreach ($services as $service) {
-                    if($specialServiceMap->has($service['serviceItem']['id'])) {
-                        DogService::updateOrCreate(['pet_id' => $petId,
-                            'service_id' => $specialServiceMap[$service['serviceItem']['id']]], [
-                            'scheduled_start' => substr($service['startDatetime'], 0, 19),
-                            // Not actually UTC because who knows
-                        ]);
+                if (isset($existingPetIds[$petId])) {
+                    foreach ($services as $service) {
+                        if ($specialServiceMap->has($service['serviceItem']['id'])) {
+                            DogService::updateOrCreate(['pet_id' => $petId,
+                                'service_id' => $specialServiceMap[$service['serviceItem']['id']]], [
+                                'scheduled_start' => substr($service['startDatetime'], 0, 19),
+                                // Not actually UTC because who knows
+                            ]);
+                        }
                     }
+                } else {
+                    Log::info('Pet ID not present: ' . $petId);
                 }
             }
         }
