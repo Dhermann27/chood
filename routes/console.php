36e6198ebaac8589a100ecf0e7b4486f3dbd329f
dhermann27 <dh78@me.com>
2026-02-18 19:53:44 -0600

    Wrong version checkin

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,6 @@ use App\Jobs\Homebase\GoFetchShiftsJob;
 use App\Jobs\Homebase\GoFetchTimecardsJob;
 use App\Jobs\MarkCabinsForCleaningJob;
 use App\Jobs\SyncDogServicesJob;
-use App\Jobs\SyncFutureReservationsJob;
 use App\Services\FetchDataService;
 use Illuminate\Support\Facades\Schedule;
 
@@ -15,7 +14,7 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('telescope:prune --hours=24')->daily();
 Schedule::job(new GoFetchListJob(app(FetchDataService::class)))->everyFifteenSeconds()->between('6:00', '19:30');
 Schedule::job(new GoFetchTimecardsJob())->everyFifteenSeconds()->between('6:00', '19:30');
-Schedule::job(new SyncFutureReservationsJob(app(FetchDataService::class)))->everyMinute()->between('6:00', '19:30');
+//Schedule::job(new SyncFutureReservationsJob(app(FetchDataService::class)))->everyMinute()->between('6:00', '19:30');
 Schedule::job(new SyncDogServicesJob())->everyMinute()->between('6:00', '19:30');
 Schedule::job(new GoFetchServiceListJob(app(FetchDataService::class)))->daily();
 Schedule::job(new MarkCabinsForCleaningJob())->daily();
